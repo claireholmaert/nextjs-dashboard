966ac64aae77eec42ee068c6e48e2a5a1feeab03
tekko <claireholmaert@gmail.com>
2024-10-20 12:27:04 +0200

    📦 NEW: update logo

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,12 +1,12 @@
 import { roboto } from "@/app/ui/fonts";
-import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import { ArchiveBoxIcon } from "@heroicons/react/24/outline";
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${roboto.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <ArchiveBoxIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Box</p>
     </div>
   );
