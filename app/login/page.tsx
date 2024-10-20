834b83e7ed900cf43ccf1d6c73d43e374c61efe9
tekko <claireholmaert@gmail.com>
2024-10-20 12:14:50 +0200

    📦 NEW: authenticate

---
+++ app/login/page.tsx
@@ -0,0 +1,17 @@
+import AcmeLogo from "../ui/acme-logo";
+import LoginForm from "../ui/login-form";
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto w-full flex max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="w-full h-20 flex items-end rounded-lg bg-indigo-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <AcmeLogo />
+          </div>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
+  );
+}
